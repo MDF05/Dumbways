93e95b2b8b38775f24f6023e129ac2d2403a3a5b
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 12:57:52 +0700

    fix: nodejs not says

--- app.mjs
+++ app.mjs
@@ -1,26 +1,4 @@
 import express from "express"
-import dotenv from "dotenv"
-import CreateError from "./utils/middleware/throwError.mjs"
-import path from "path"
-import ejs from "ejs"
-import expressEjsLayouts from "express-ejs-layouts"
-
-import homeRouter from "./route/home-router.mjs"
-import contactRouter from "./route/contact-router.mjs"
-import testimoniRouter from "./route/testimoni-router.mjs"
-import myProjectRouter from "./route/myproject-router.mjs"
-
-dotenv.config()
-const app = express()
-const port = process.env.port || 3000
-
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(expressEjsLayouts)
-app.use("/assets", express.static("assets"))
-app.set("view engine", ejs)
-app.set("views", "views")
-app.set("view cache", true)
 
 app.get("/", (req, res, next) => {
     return res.json({
@@ -47,4 +25,4 @@ app.use((err, req, res, next) => {
     })
 })
 
-app.listen(port, () => console.log(`your app listening on http://localhost:${port}`))
+app.listen(3000, () => console.log(`your app listening on http://localhost:${port}`))
