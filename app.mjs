3018d97ddc4fc0bc60b3b638689578d718c21056
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 15:21:13 +0700

    fix: ejs not display

--- app.mjs
+++ app.mjs
@@ -15,11 +15,12 @@ const port = process.env.port || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(expressEjsLayouts)
 app.use("/assets", express.static("assets"))
 app.set("view engine", "ejs")
 app.set("views", "views")
 app.set("view cache", true)
+
+app.use(expressEjsLayouts)
 
 app.get("/", (req, res, next) => {
     return res.json({
