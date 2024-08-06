a4f5a6e2ecedaa425b3e6f2ac0c6838da27cfb92
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 13:25:59 +0700

    fix: crashed deployment 3

--- app.mjs
+++ app.mjs
@@ -1,21 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
-import CreateError from "./utils/middleware/throwError.mjs"
-import path from "path"
-import ejs from "ejs"
-import expressEjsLayouts from "express-ejs-layouts"
 
-import homeRouter from "./route/home-router.mjs"
-import contactRouter from "./route/contact-router.mjs"
-import testimoniRouter from "./route/testimoni-router.mjs"
-import myProjectRouter from "./route/myproject-router.mjs"
 
 dotenv.config()
 const app = express()
 const port = process.env.port || 3000
-export const version = "v1"
 
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(expressEjsLayouts)
+app.use("/assets", express.static("assets"))
+app.set("view engine", ejs)
+app.set("views", "views")
+app.set("view cache", true)
 
 app.get("/", (req, res, next) => {
     return res.json({
@@ -25,10 +22,6 @@ app.get("/", (req, res, next) => {
     })
 })
 
-app.use(`/${version}/home`, homeRouter)
-app.use(`/${version}/contact`, contactRouter)
-app.use(`/${version}/testimoni`, testimoniRouter)
-app.use(`/${version}/myproject`, myProjectRouter)
 
 app.use("/", (req, res, next) => {
     return next(CreateError(404, "page not found"))
