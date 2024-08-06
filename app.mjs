57bb4ce1b531fc04c5d74afbd6f798339c4cd764
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 12:41:56 +0700

    fix: app.mjs strukture baru 7

--- app.mjs
+++ app.mjs
@@ -4,13 +4,11 @@ import ejs from "ejs"
 import expressEjsLayouts from "express-ejs-layouts"
 
 const app = express()
-export const version = "v1"
 
 app.get("/", (req, res, next) => {
     return res.json({
         author: "muhammad dava fahreza",
         succes: true,
-        version,
     })
 })
 
