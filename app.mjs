5fa3562d19e235f0f3e47e88035b64a6e00f5baa
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 12:27:46 +0700

    fix: app.mjs strukture baru

--- app.mjs
+++ app.mjs
@@ -5,11 +5,6 @@ import path from "path"
 import ejs from "ejs"
 import expressEjsLayouts from "express-ejs-layouts"
 
-import homeRouter from "./route/home-router.mjs"
-import contactRouter from "./route/contact-router.mjs"
-import testimoniRouter from "./route/testimoni-router.mjs"
-import myProjectRouter from "./route/myproject-router.mjs"
-
 dotenv.config()
 const app = express()
 export const version = "v1"
@@ -18,7 +13,7 @@ app.get("/", (req, res, next) => {
     return res.json({
         author: "muhammad dava fahreza",
         succes: true,
-        version: version,
+        version,
     })
 })
 
