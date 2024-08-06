ab813a6cd1fe5f23b6054b51935724f9fe99559e
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 13:33:29 +0700

    fix: deployment crashed testing 1

--- app.mjs
+++ app.mjs
@@ -1,10 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
+import CreateError from "./utils/middleware/throwError.mjs"
+import path from "path"
+import expressEjsLayouts from "express-ejs-layouts"
 
 
 dotenv.config()
 const app = express()
 const port = process.env.port || 3000
+export const version = "v1"
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -17,6 +21,7 @@ app.get("/", (req, res, next) => {
     return res.json({
         author: "muhammad dava fahreza",
         succes: true,
+        version
     })
 })
 
