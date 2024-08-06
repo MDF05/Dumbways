bba7621bedaed43ab337ab44c7716d60a6a7fade
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 14:14:36 +0700

    fix: test export versoin

--- app.mjs
+++ app.mjs
@@ -7,7 +7,7 @@ import expressEjsLayouts from "express-ejs-layouts"
 dotenv.config()
 const app = express()
 const port = process.env.port || 3000
-const version = "v1"
+export const version = "v1"
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
