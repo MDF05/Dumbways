5fc782769e7d2030c010d939dbf5af65a811921d
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-08-06 12:22:32 +0700

    fix: app.mjs strukture

--- app.mjs
+++ app.mjs
@@ -1,11 +1,25 @@
 import express from "express"
+import dotenv from "dotenv"
+import CreateError from "./utils/middleware/throwError.mjs"
+import path from "path"
+import ejs from "ejs"
+import expressEjsLayouts from "express-ejs-layouts"
 
+import homeRouter from "./route/home-router.mjs"
+import contactRouter from "./route/contact-router.mjs"
+import testimoniRouter from "./route/testimoni-router.mjs"
+import myProjectRouter from "./route/myproject-router.mjs"
+
+dotenv.config()
 const app = express()
+const port = process.env.port || 3000
+export const version = "v1"
 
 app.get("/", (req, res, next) => {
     return res.json({
         author: "muhammad dava fahreza",
         succes: true,
+        version: version,
     })
 })
 
